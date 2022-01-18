2efa55c08c56465086106967216e1c760abbe001
Maria Kirilova <47693680+mariakirilova2004@users.noreply.github.com>
2022-01-18 21:52:04 +0200

    Delete Objects-and-Composition-Lab directory

--- Objects-and-Composition-Lab/city_record.js
+++
@@ -1,6 +0,0 @@
-function city_record(name, population, tresury){
-    let City = {name: name, population: population, treasury: tresury};
-    return City;
-}
-
-console.log(city_record('Tortuga', 7000, 15000 ));
