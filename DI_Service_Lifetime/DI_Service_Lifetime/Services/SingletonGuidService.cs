e1d92483a27afea362b3c90bf774761e8aed8379
Maria Kirilova <47693680+mariakirilova2004@users.noreply.github.com>
2023-06-05 13:14:03 +0300

    Add files via upload

---
+++ DI_Service_Lifetime/DI_Service_Lifetime/Services/SingletonGuidService.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Sockets;
+
+namespace DI_Service_Lifetime.Services
+{
+    public class SingletonGuidService : ISingletonGuidService
+    {
+        private readonly Guid Id;
+
+        public SingletonGuidService()
+        {
+            Id = Guid.NewGuid();
+        }
+        public string GetGuid()
+        {
+            return Id.ToString();
+        }
+    }
+}
